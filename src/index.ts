e03a60c7159c4d7b1c894819ebb95cbcba6b73c4
Andy Wright <atwright147@gmail.com>
2024-02-22 22:29:54 +0000

    install and use biome

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import path from 'path';
 import sharp from 'sharp';
 import randomColor from 'randomcolor';
 
-// ×
-
 const sizes = [
   [4608, 2592],
   [6000, 4000],
@@ -18,10 +16,9 @@ fs.mkdirSync('images', { recursive: true });
 let index = 1;
 
 for (const size of [...sizes, ...sizes]) {
-  // Generate a random HSL background color
   const bgColor = randomColor({
-    format: 'hsl', // Generate color in HSL format
-    luminosity: 'light', // Ensure the color is light enough for text readability
+    format: 'hsl',
+    luminosity: 'light',
   });
 
   let orientation = 'L';
@@ -32,41 +29,40 @@ for (const size of [...sizes, ...sizes]) {
   }
   const indexString = `${orientation}${String(index).padStart(2, '0')}`;
 
-  // Calculate font sizes based on the image dimensions
+  // calculate font sizes based on the image dimensions
   const fontSize1 = Math.min(width, height) * 0.18;
   const fontSize2 = fontSize1 * 0.67;
 
-  // Create a new image with the specified dimensions and background color
   sharp({
     create: {
       width: width,
       height: height,
       channels: 3,
       background: bgColor,
-    }
+    },
   })
-  // Composite the image with text displaying its dimensions
-  .composite([{
-    input: Buffer.from(`
-      <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
-        <text x="50%" y="43%" dominant-baseline="middle" text-anchor="middle" fill="ashgray" font-family="Rockwell" font-size="${fontSize1}">
-          ${indexString}
-        </text>
-        <text x="50%" y="57%" dominant-baseline="middle" text-anchor="middle" fill="ashgray" font-family="Rockwell" font-size="${fontSize2}">
-          ${width}×${height}
-        </text>
-      </svg>`
-    ),
-    gravity: 'center',
-  }])
-  // Save the final image
-  .toFile(path.join('images', `${indexString}-${width}x${height}.png`), (err, info) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Image generated:', info);
-    }
-  });
+    .composite([
+      {
+        input: Buffer.from(`
+          <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}">
+            <text x="50%" y="43%" dominant-baseline="middle" text-anchor="middle" fill="ashgray" font-family="Rockwell" font-size="${fontSize1}">
+              ${indexString}
+            </text>
+            <text x="50%" y="57%" dominant-baseline="middle" text-anchor="middle" fill="ashgray" font-family="Rockwell" font-size="${fontSize2}">
+              ${width}×${height}
+            </text>
+          </svg>
+        `),
+        gravity: 'center',
+      },
+    ])
+    .toFile(path.join('images', `${indexString}-${width}x${height}.png`), (err, info) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Image generated:', info);
+      }
+    });
 
   index += 1;
 }
